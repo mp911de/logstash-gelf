5be6afb78bd8649535490484c1f3c43cfac077bb
Mark Paluch <mpaluch@paluch.biz>
2014-07-17 16:31:32 +0200

    Added closer to centralize IOException handling/Java6 fix

--- src/main/java/biz/paluch/logging/gelf/intern/Closer.java
+++ src/main/java/biz/paluch/logging/gelf/intern/Closer.java
@@ -2,6 +2,7 @@ package biz.paluch.logging.gelf.intern;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.net.Socket;
 
 /**
  * @author <a href="mailto:mark.paluch@1und1.de">Mark Paluch</a>
@@ -18,6 +19,21 @@ public class Closer {
         try {
             if (closeable != null) {
                 closeable.close();
+            }
+        } catch (IOException e) {
+            // ignore
+        }
+    }
+
+    /**
+     * Close silently the socket.
+     * 
+     * @param socket
+     */
+    public final static void close(Socket socket) {
+        try {
+            if (socket != null) {
+                socket.close();
             }
         } catch (IOException e) {
             // ignore
