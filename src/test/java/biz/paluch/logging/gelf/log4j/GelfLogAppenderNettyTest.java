4a75122ad07b16a2c1b682f72f5a42b629606c51
Mark Paluch <mpaluch@paluch.biz>
2015-03-19 21:15:52 +0100

    Extend tests for log4j
    Add tests that use GELF over UDP with netty backend and threshold WARN on appender and the logging category itself.

---
+++ src/test/java/biz/paluch/logging/gelf/log4j/GelfLogAppenderNettyTest.java
@@ -0,0 +1,104 @@
+package biz.paluch.logging.gelf.log4j;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeoutException;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import biz.paluch.logging.gelf.NettyLocalServer;
+
+import com.google.code.tempusfugit.temporal.*;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+/**
+ * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
+ */
+public class GelfLogAppenderNettyTest {
+
+    private static NettyLocalServer server = new NettyLocalServer(NioDatagramChannel.class);
+
+    @BeforeClass
+    public static void setupClass() throws Exception {
+        server.run();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        server.close();
+    }
+
+    @Before
+    public void before() throws Exception {
+        LogManager.getLoggerRepository().resetConfiguration();
+        DOMConfigurator.configure(getClass().getResource("/log4j-netty-warn.xml"));
+        server.clear();
+    }
+
+    private void waitForGelf(final int minMessageCount) throws InterruptedException, TimeoutException {
+        WaitFor.waitOrTimeout(new Condition() {
+            @Override
+            public boolean isSatisfied() {
+                return server.getJsonValues().size() >= minMessageCount;
+            }
+        }, Timeout.timeout(Duration.seconds(2)));
+    }
+
+    @Test
+    public void testLogInfoLimitedCategory() throws Exception {
+        Logger logger = Logger.getLogger("org.something.else");
+        logger.info("info1");
+        logger.info("info2");
+        logger.info("info3");
+        logger.info("info4");
+
+        new ThreadSleep(Duration.seconds(2)).sleep();
+
+        assertEquals(0, server.getJsonValues().size());
+    }
+
+    @Test
+    public void testLogWarnLimitedCategory() throws Exception {
+        Logger logger = Logger.getLogger("org.something.else");
+        logger.warn("warn1");
+        logger.warn("warn2");
+        logger.warn("warn3");
+        logger.warn("warn4");
+
+        waitForGelf(4);
+
+        assertEquals(4, server.getJsonValues().size());
+    }
+
+    @Test
+    public void testLogInfo() throws Exception {
+        Logger logger = Logger.getLogger("mylog");
+        logger.info("info1");
+        logger.info("info2");
+        logger.info("info3");
+        logger.info("info4");
+
+        new ThreadSleep(Duration.seconds(2)).sleep();
+
+        assertEquals(0, server.getJsonValues().size());
+    }
+
+    @Test
+    public void testLogWarn() throws Exception {
+        Logger logger = Logger.getLogger("mylog");
+        logger.warn("warn1");
+        logger.warn("warn2");
+        logger.warn("warn3");
+        logger.warn("warn4");
+
+        waitForGelf(4);
+
+        assertEquals(4, server.getJsonValues().size());
+    }
+}
