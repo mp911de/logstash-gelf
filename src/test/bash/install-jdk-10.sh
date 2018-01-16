b64943488d147351fbec661c873bfd054c3061ea
Mark Paluch <mpaluch@paluch.biz>
2018-01-16 11:37:58 +0100

    Adapt to changed JDK 10 download URL #124

--- src/test/bash/install-jdk-10.sh
+++ src/test/bash/install-jdk-10.sh
@@ -2,11 +2,11 @@
 set -e
 
 JDK_FEATURE=10
-JDK_BUILD=30
+JDK_BUILD=39
 JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
-wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/binaries/${JDK_ARCHIVE}
+wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/BCL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
