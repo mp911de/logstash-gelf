44700049f4f57c530c2881aa5abfff0695788530
Mark Paluch <mpaluch@paluch.biz>
2018-01-29 16:09:01 +0100

    Upgrade to JDK 10b41 #124

--- src/test/bash/install-jdk-10.sh
+++ src/test/bash/install-jdk-10.sh
@@ -2,11 +2,11 @@
 set -e
 
 JDK_FEATURE=10
-JDK_BUILD=40
-JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+JDK_BUILD=41
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
-wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/BCL/${JDK_ARCHIVE}
+wget https://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
