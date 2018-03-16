dd17356db6bd8e1b40444bf0c4d282f7866ab2e2
Mark Paluch <mpaluch@paluch.biz>
2018-03-16 08:24:26 +0100

    Upgrade to JDK 10b46 #124

--- src/test/bash/install-jdk-10.sh
+++ src/test/bash/install-jdk-10.sh
@@ -2,8 +2,8 @@
 set -e
 
 JDK_FEATURE=10
-JDK_BUILD=41
-JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+JDK_BUILD=46
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
 wget https://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
